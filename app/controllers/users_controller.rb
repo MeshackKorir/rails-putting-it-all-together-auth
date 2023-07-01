055c4510fd382347db1b4120996ebc30f12cc7ba
Meshack Korir <meshackkorir100@gmail.com>
2023-07-01 23:50:27 +0300

    updated user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
-end
+    def create
+      user = User.new(user_params)
+  
+      if user.save
+        session[:user_id] = user.id
+        render json: user, status: :created, except: :password_digest
+      else
+        render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  
+    def show
+      if logged_in?
+        render json: current_user, except: :password_digest
+      else
+        render json: { error: 'Unauthorized' }, status: :unauthorized
+      end
+    end
+  
+    private
+  
+    def user_params
+      params.require(:user).permit(:username, :password, :image_url, :bio)
+    end
+  end
+  
