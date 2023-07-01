a5cac21fba45b4037cbe723816393c8d80d26944
Meshack Korir <meshackkorir100@gmail.com>
2023-07-01 23:56:31 +0300

    updated session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
-end
+    def create
+      user = User.find_by(username: params[:username])
+  
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        render json: user, except: :password_digest
+      else
+        render json: { error: 'Unauthorized' }, status: :unauthorized
+      end
+    end
+  
+    def destroy
+      if logged_in?
+        session.delete(:user_id)
+        head :no_content
+      else
+        render json: { error: 'Unauthorized' }, status: :unauthorized
+      end
+    end
+  end
+  
